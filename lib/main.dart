e84da45aa76a9d3d131e10c2f663030d76cc5046
FaatihahMa04 <faatihahmaurisna04@gmail.com>
2025-01-17 16:11:53 +0700

    hero

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,50 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Wisata Bandung",
-      theme: ThemeData(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       //home : const DetailScreen(),
-      home: MainScreen(),
+      //home: MainScreen(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hero animation'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: "logo",
+              child: Image.asset(
+                'images/dicoding.png',
+                width: 150,
+                height: 150,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                child: Text('Next Screen'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return MainScreen();
+                  }));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
